1cd086bd24e199f32eafa25ab82f523d0e7eab50
Katiktus <katpog2001@gmail.com>
2022-01-31 18:38:07 +0200

    add login authorization

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://katiktus:16022001@cluster0.nsfo6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://katiktus:16022001@cluster0.nsfo6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
